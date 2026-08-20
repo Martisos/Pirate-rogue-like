da91cbb6472c642684d89f8a8068bd4450e48816
Martisos <gromar6666@gmail.com>
2026-08-21 00:38:02 +0200

    Enemies Shoot Fix
    Now enemies turn to you and shoot only from one side

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,11 +4,13 @@ extends CharacterBody2D
 @export var max_health: int = 3
 @export var attack_range: float = 600.0
 @export var attack_cooldown: float = 2.0
+@export var turn_speed: float = 2.0
 @export var cannonball_scene : PackedScene = preload("uid://c3c8elea3mokb")
 
 var health: int
 var player = null
 var can_shoot: bool = true
+var choosen_side: String = ""
 
 @onready var shoot_cooldown: Timer = $ShootCooldown
 @onready var cannons_node: Node2D = $Cannons
@@ -25,14 +27,36 @@ func _physics_process(delta: float) -> void:
 		return
 	
 	var distance_to_player = global_position.distance_to(player.global_position)
-	look_at(player.global_position)
+	
+	var target_direction = global_position.direction_to(player.global_position)
+	var target_angle = target_direction.angle()
+	
+	if distance_to_player <= attack_range:
+		if choosen_side == "":
+			var enemy_forward = Vector2.RIGHT.rotated(rotation)
+			var cross_product = enemy_forward.cross(target_direction)
+			
+			if cross_product < 0:
+				choosen_side = "left"
+			else:
+				choosen_side = "right"
+			
+				
+		if choosen_side == "left":
+			target_angle += PI / 2.0
+		else:
+			target_angle -= PI / 2.0
+	else:
+		choosen_side = ""
+		
+	rotation = lerp_angle(rotation, target_angle, turn_speed * delta)
 	
 	#attacks and movement
 	if distance_to_player > attack_range:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * speed
+		velocity = Vector2.RIGHT.rotated(rotation) * speed
 		move_and_slide()
 	else:
+		
 		velocity = Vector2.ZERO
 		
 		if can_shoot:
@@ -42,14 +66,23 @@ func shoot() -> void:
 	if cannonball_scene != null and cannons_node != null:
 		can_shoot = false
 		
+		var shot_left = (choosen_side == "left")
+		
 		for cannon in cannons_node.get_children():
-			var ball = cannonball_scene.instantiate()
-			ball.shooter = self
-			get_tree().root.add_child(ball)
 			
-			ball.global_position = cannon.global_position
-			ball.rotation = rotation
-			
+			if cannon.name.begins_with("L") and shot_left:
+				var ball = cannonball_scene.instantiate()
+				ball.shooter = self
+				get_tree().root.add_child(ball)
+				ball.global_position = cannon.global_position
+				ball.rotation = rotation - (PI / 2.0)
+			elif cannon.name.begins_with("R") and not shot_left:
+				var ball = cannonball_scene.instantiate()
+				ball.shooter = self
+				get_tree().root.add_child(ball)
+				ball.global_position = cannon.global_position
+				ball.rotation = rotation + (PI / 2.0)
+				
 			shoot_cooldown.start(attack_cooldown)
 
 func take_damage(amount: int) -> void:
@@ -60,7 +93,6 @@ func take_damage(amount: int) -> void:
 func die():
 	#fancy something after enemy dies
 	queue_free()
-
 
 func _on_shoot_cooldown_timeout() -> void:
 	can_shoot = true
--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,20 +1,46 @@
 extends Node
 
-@onready var enemy_scene: PackedScene = preload("uid://dwqjqohfahwf7")
-@onready var spawn_timer: Timer = $SpawnTimer
-@onready var player: CharacterBody2D = $"../Player"
+#0 mini
+#1 small
+#2 medium
+#3 large
+@export var enemies: Array[PackedScene]
 
+@onready var spawn_timer: Timer = $SpawnTimer
+
+var player = null
+
+func _ready() -> void:
+	var players = get_tree().get_nodes_in_group("player")
+	if players.size() > 0:
+		player = players[0]
 
 func _on_spawn_timer_timeout() -> void:
-	if enemy_scene == null or !player:
+	var chosen_enemy_scene: PackedScene
+	
+	var randomNumber = randi_range(0, 10)
+	
+	if randomNumber in range(0,5):
+		chosen_enemy_scene = enemies[0]
+		print("mini", randomNumber)
+	elif randomNumber in range(5,8):
+		chosen_enemy_scene = enemies[1]
+		print("small", randomNumber)
+	elif randomNumber in range(8,10):
+		chosen_enemy_scene = enemies[2]
+		print("medium", randomNumber)
+	if randomNumber == 10:
+		chosen_enemy_scene = enemies[3]
+		print("large", randomNumber)
+	
+	if chosen_enemy_scene == null:
 		return
 	
-	var enemy = enemy_scene.instantiate()
-	
+	var enemy = chosen_enemy_scene.instantiate()
 	var angle = randf() * TAU
-	var distance = randf_range(1000, 3000)
+	var distance = randf_range(1200, 3500)
 	
 	var spawn_offset = Vector2(cos(angle), sin(angle)) * distance
 	enemy.global_position = player.global_position + spawn_offset
 	
-	get_tree().root.add_child(enemy)
+	get_tree().current_scene.add_child(enemy)
