18d3ec72dcba6de5e7506d6c6c608a4c00c2b8b8
Martisos <gromar6666@gmail.com>
2026-08-20 20:30:51 +0200

    Better movement
    changed the movement of player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,26 @@
 extends CharacterBody2D
 
 const speed = 200.0
+const turn_speed = 3.0
 
-var target = position
+var target: Vector2
+
+func _ready() -> void:
+	target = global_position
 
 func _input(event) -> void:
 	if event.is_action_pressed("click"):
 		target = get_global_mouse_position()
 
 func _physics_process(delta: float) -> void:
-	velocity = position.direction_to(target) * speed
-	look_at(target)
 	if position.distance_to(target) > 10:
+		var direction_to_target = global_position.direction_to(target)
+		var target_angle = direction_to_target.angle()
+		
+		rotation = lerp_angle(rotation, target_angle, turn_speed * delta)
+		
+		velocity = Vector2.RIGHT.rotated(rotation) * speed
+		
 		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
